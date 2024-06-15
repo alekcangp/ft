bbc4176563965ab37f444f0402087543b03cb209
alekcangp <40195312+alekcangp@users.noreply.github.com>
2024-06-15 09:56:06 +0600

    Update Inspector.tsx

--- templates/gifmaker/Inspector.tsx
+++ templates/gifmaker/Inspector.tsx
@@ -2,10 +2,14 @@
 import { Button } from '@/components/shadcn/Button'
 import { Input } from '@/components/shadcn/Input'
 import { useFrameConfig, useFrameId, useUploadImage } from '@/sdk/hooks'
-import { useRef,useState,useEffect } from 'react'
+import { useRef, useState,useEffect } from 'react'
 import type { Config } from '.'
 import { FFmpeg } from '@ffmpeg/ffmpeg'
 import { fetchFile, toBlobURL } from '@ffmpeg/util'
+
+
+
+
 
 export default function Inspector() {
     const frameId = useFrameId()
@@ -14,98 +18,121 @@ export default function Inspector() {
     const imageRef = useRef<HTMLImageElement | null>(null)
     const videoRef = useRef<HTMLVideoElement | null>(null)
     const logs = useRef(null)
-    const ffmpegRef = useRef(new FFmpeg())
     const [file, setFile] = useState<File>()
- 
+    const ffmpegRef = useRef(new FFmpeg())
+     
+
     const load = async () => {
-      
+       
         const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
         const ffmpeg = ffmpegRef.current
+       
         // toBlobURL is used to bypass CORS issue, urls with the same
         // domain can be used directly.
         await ffmpeg.load({
           coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
           wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm')
         })
-        logs.current.value = 'Ready!'
+       
       }
 
       setTimeout(load,1000)
 
-      const transcode = async () => {
-        try {
-            logs.current.value = 'Downloading video file and creating GIF . . .'
-              const ffmpeg = ffmpegRef.current
-              const ty = file.type.substring(file.type.indexOf('/')+1)
-              // u can use 'https://ffmpegwasm.netlify.app/video/video-15s.avi' to download the video to public folder for testing
-              await ffmpeg.writeFile(`input.${ty}`, await fetchFile(file))
-            // const vurl = await toBlobURL('https://file-examples.com/wp-content/storage/2018/04/file_example_AVI_480_750kB.avi', 'video/avi')
-            // await ffmpeg.writeFile('input.avi', await fetchFile(vurl))
-            //"-vf","drawtext=fontfile=/arial.ttf:text=Artist:fontcolor=white:fontsize=24:x=(w-text_w)/2:y=(h-text_h)/2",
-              await ffmpeg.writeFile('arial.ttf', await fetchFile('https://raw.githubusercontent.com/alekcangp/frametrain/master/arial.ttf'))
-              ffmpeg.exec(["-i",`input.${ty}`,"-ss","0","-t","10","-r","10","-vf","scale=-1:250,drawtext=fontfile=arial.ttf:text='sdfdsfsd sdfsd':fontcolor=green:fontsize=24:x=(w-text_w)/2:y=(h-text_h)/2","output.gif"]); //:x=(w-text_w)/2:y=(h-text_h)/2
-              const data = (await ffmpeg.readFile('output.gif')) as any
-              var b64 = Buffer.from(data).toString('base64');
-             
-             // const { filePath } = await uploadImage({
-             //    base64String: b64,
-              //    contentType: 'image/gif',
-             // })
-             // const gifUrl = process.env.NEXT_PUBLIC_CDN_HOST+'/'+filePath;
-             // console.log(gifUrl);
-               logs.current.value = `Successfully!`;// Gif's url: ${gifUrl}`
-      
-              if (imageRef.current){
-                  const urll = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
-                 // console.log(urll)
-                  imageRef.current.src = urll
-                
-              }
-          }catch(e) {
-              logs.current.value = `Something went wrong: ${JSON.stringify(e)}`
-          }
-              
-             
-      }
-
-    const { gif, video, link, label, start, finish, caption, fontsize, fps, scale } = config
-
-    const inputVideoUrl = useRef<HTMLInputElement>(null)
     const inputStart = useRef<HTMLInputElement>(null)
-    const inputFinish = useRef<HTMLInputElement>(null)
+    const inputDuration = useRef<HTMLInputElement>(null)
     const inputCaption = useRef<HTMLInputElement>(null)
     const inputFontSize = useRef<HTMLInputElement>(null)
-    const inputFps = useRef<HTMLInputElement>(null)
-    const inputScale = useRef<HTMLInputElement>(null)
+    const inputFontColor = useRef<HTMLInputElement>(null)
+    const inputY = useRef<HTMLInputElement>(null)
     const inputButtonLabel = useRef<HTMLInputElement>(null)
     const inputButtonLink = useRef<HTMLInputElement>(null)
-    
 
-    const confDefault = {
+const confDefault = {
         gif: 'https://i.postimg.cc/fLRwTKnF/roboto.gif',
-        video: 'https://www.youtube.com/watch?v=DYCIlghl5rI',
-        start: 15,
-        finish: 20,
+        start: '0',
+        duration: '10',
         caption: 'Hello from FrameTrain',
-        fontsize: 20,
-        fps: 10,
-        scale: 320,
-        label: 'VIEW',
+        fontsize: '30',
+        fontcolor: 'white',
+        y: '10',
+        label: 'Do It',
+        link: 'https://frametra.in'
     }
 
-    
-    useEffect(() => {
+      var data:any=null, params={};
+   
+      const transcode = async () => {
+        try {
+            logs.current.value = 'Creating GIF. Please wait. . .'
+            params = {           
+                start: inputStart.current?.value.toString() || confDefault.start,
+                duration: inputDuration.current?.value.toString() || confDefault.duration,
+                caption: inputCaption.current?.value || confDefault.caption,
+                y: inputY.current?.value.toString() || confDefault.y,
+                fontsize: inputFontSize.current?.value.toString() || confDefault.fontsize,
+                fontcolor: inputFontColor.current?.value || confDefault.fontcolor,
+                label: inputButtonLabel.current?.value || confDefault.label,
+                link: inputButtonLink.current?.value || confDefault.link
+            }
+            console.log(params)
+              const ffmpeg = ffmpegRef.current
+              const ty = file.type.substring(file.type.indexOf('/')+1)
+              await ffmpeg.writeFile(`input.${ty}`, await fetchFile(file))
+              await ffmpeg.writeFile('arial.ttf', await fetchFile('https://ipfs.io/ipfs/QmcWFzXah4SAbKruiDjUN9rgSzztDwJU4F9d6qreRZNF1C'))// font 
+              //ffmpeg.exec(["-i",`input.${ty}`,"-ss",`"${params.start}"`,"-t",`"${params.duration}"`,"-r","10","-vf",`"scale=-1:250,drawtext=fontfile=arial.ttf:text='${params.caption}':fontcolor=${params.fontcolor}:fontsize=${params.start}:x=${params.x}:y=${params.y}"`,"output.gif"]); //:x=(w-text_w)/2:y=(h-text_h)/2
+              ffmpeg.exec(["-i",`input.${ty}`,"-ss",params.start,"-t",params.duration,"-r","10","-vf",`scale=-1:250,drawtext=fontfile=arial.ttf:text='${params.caption}':fontcolor=${params.fontcolor}:fontsize=${params.fontsize}:x=(w-text_w)/2:y=(h-text_h)-${params.y}`,"output.gif"]); //:x=(w-text_w)/2:y=(h-text_h)/2
+              
+              data = (await ffmpeg.readFile('output.gif'))
+              if (imageRef.current){
+                  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+                  imageRef.current.src = url
+              }
+               logs.current.value = `Successfully!`;
+
+          }catch(e) {
+              logs.current.value = `Something went wrong. Upload a file and check the params. ${JSON.stringify(e)}`
+          }
+                  
+      }
+
+   const create = async () => {
+        try { 
+        if (!data) {logs.current.value = 'Preview not found. Hit "Privew" button.'; return}
+        //updateConfig({ gif: confDefault.gif });
+        logs.current.value = 'Creating Frame . . .'
+
+        const b64 = Buffer.from(data).toString('base64');
+        const { filePath } = await uploadImage({base64String: b64,contentType: 'image/gif'})
+        const gifUrl = process.env.NEXT_PUBLIC_CDN_HOST+'/'+filePath;
+        console.log(gifUrl);
+        updateConfig({
+            gif: gifUrl,
+            label: params.label,
+            link: params.link,
+            params: params
+        })
+        logs.current.value = 'Successfully!'
+    } catch (e) {
+        logs.current.value = 'Something went wrong. Perhaps, the GIF is too large. Check params and try again.'
+    }
+}
+
+
+useEffect(() => {
         if (!file) return
-        videoRef.current.src = URL.createObjectURL(file); 
-        console.log(file.name)
+          videoRef.current.src = URL.createObjectURL(file);
     }, [file])
 
+       
     return (
+
+        
         <div className="w-full h-full space-y-4">
+            
             <h1 className="text-lg font-semibold">GIF Maker</h1>
 
-
             <video ref={videoRef} width="100%" controls></video>
+          
             <label
                         htmlFor="uploadFile"
                         className="flex cursor-pointer items-center justify-center rounded-md  py-1.5 px-2 text-md font-medium bg-border  text-primary hover:bg-secondary-border"
@@ -125,23 +152,20 @@ export default function Inspector() {
                         />
                     </label>
 
+
             <h3 className="text-lg font-semibold">Enter parameters</h3>
 
             <div className="flex flex-col gap-2 ">
+               
                 <Input
                     className="text-lg"
-                    placeholder="Youtube URL (default: https://www.youtube.com/watch?v=DYCIlghl5rI)"
-                    ref={inputVideoUrl}
-                />
-                <Input
-                    className="text-lg"
-                    placeholder="Start time in sec (default: 15)"
+                    placeholder="Start time in sec or mm:ss (default: 0)"
                     ref={inputStart}
                 />
                 <Input
                     className="text-lg"
-                    placeholder="Finish time in sec (default: 20)"
-                    ref={inputFinish}
+                    placeholder="Duration in sec (default: 10)"
+                    ref={inputDuration}
                 />
                 <Input
                     className="text-lg"
@@ -150,84 +174,46 @@ export default function Inspector() {
                 />
                 <Input
                     className="text-lg"
-                    placeholder="Font size (default: 20)"
-                    ref={inputFontSize}
-                />
-                <Input className="text-lg"
-                     placeholder="FPS (default: 10)"
-                     ref={inputFps}
-                />
-                <Input className="text-lg"
-                     placeholder="Scale (default: 320)"
-                     ref={inputScale}
+                    placeholder="Position Y (default: 10)"
+                    ref={inputY}
                 />
                 <Input
                     className="text-lg"
-                    placeholder="Button label (default: VIEW)"
+                    placeholder="Font size (default: 30)"
+                    ref={inputFontSize}
+                />
+                <Input className="text-lg"
+                     placeholder="Font color (default: white)"
+                     ref={inputFontColor}
+                />
+                <Input
+                    className="text-lg"
+                    placeholder="Button label (default: LINK)"
                     ref={inputButtonLabel}
                 />
                 <Input
                     className="text-lg"
-                    placeholder="Button link (default: Video URL + Start time)"
+                    placeholder="Button link (default: https://frametra.in)"
                     ref={inputButtonLink}
                 />
-                <img ref={imageRef} ></img>
-                <br />
-                <button onClick={transcode} className="bg-green-500 hover:bg-green-700 text-white py-3 px-6 rounded">
-                 Preview
-                </button>
+
+<img ref={imageRef} width="100%" ></img>
+      <br />
+      <button
+        onClick={transcode}
+        className="bg-green-500 hover:bg-green-700 text-white py-3 px-6 rounded"
+      >
+        Preview
+      </button>
+
                 Console: <textarea style={{ color: '#00FFFF' }} ref={logs}></textarea>
                 <p>
-                Note: The recommended duration of the gif is less 10 sec.
-
+                Note: The recommended gif's duration is less 10 sec.
             </p>
+
                 <Button
-                    onClick={async () => {
-                        const params = {
-                            video: inputVideoUrl.current?.value || confDefault.video,
-                            start: inputStart.current?.value || confDefault.start,
-                            finish: inputFinish.current?.value || confDefault.finish,
-                            caption: inputCaption.current?.value || confDefault.caption,
-                            fontsize: inputFontSize.current?.value || confDefault.fontsize,
-                            fps: inputFps.current?.value || confDefault.fps,
-                            scale: inputScale.current?.value || confDefault.scale,
-                            label: inputButtonLabel.current?.value || confDefault.label,
-                            link: inputButtonLink.current?.value || inputVideoUrl.current?.value + '#t=' + inputStart.current?.value,
-                        }
+                    onClick={create}
 
-                        //updateConfig({ gif: confDefault.gif });
-                        logs.current.value = 'Downloading video and creating Gif. Please, wait . . .'
-
-                        //fetch gif url ...
-                        try {
-                            const resp = await fetch(
-                                `https://87.251.66.40/api?url=${params.video}&start=${params.start}&finish=${params.finish}&caption="${params.caption}"&fontsize=${params.fontsize}&fps=${params.fps}&scale=${params.scale}`
-                            )
-                            const data = await resp.json()
-                            console.log(data.url)
-
-                            if (data.url == confDefault.gif) {
-                                logs.current.value = 'Something went wrong. Check params and try again.'
-                            } else {
-                                logs.current.value = `Complited! ${data.url}`
-                            }
-
-                            updateConfig({
-                                gif: data.url,
-                                video: params.video,
-                                start: params.start,
-                                finish: params.finish,
-                                caption: params.caption,
-                                fontsize: params.fontsize,
-                                fps: params.fps,
-                                scale: params.scale,
-                                label: params.label,
-                                link: params.link,
-                            })
-                        } catch (e) {
-                            logs.current.value = `Something went wrong. Check params and try again. ${e}`
-                        }
-                    }}
                     className="w-full bg-border hover:bg-secondary-border text-primary"
                 >
                 Create GIF
@@ -237,45 +223,20 @@ export default function Inspector() {
             <Button
                 className="w-full"
                 onClick={() => {
-
-                    inputVideoUrl.current.value = config.video || confDefault.video
-                    inputStart.current.value = config.start || confDefault.start
-                    inputFinish.current.value = config.finish || confDefault.finish
-                    inputCaption.current.value = config.caption || confDefault.caption
-                    inputFontSize.current.value = config.fontsize || confDefault.fontsize
-                    inputFps.current.value = config.fps || confDefault.fps
-                    inputScale.current.value = config.scale || confDefault.scale
-                    inputButtonLabel.current.value = config.label || confDefault.label
-                    inputButtonLink.current.value = config.link || confDefault.video + '#t=' + confDefault.start;
-
-                    logs.current.value = 'Success!'
+                    const { params } = config
+                    inputStart.current.value = config.params?.start || confDefault.start
+                    inputDuration.current.value = config.params?.duration || confDefault.duration
+                    inputCaption.current.value = config.params?.caption || confDefault.caption
+                    inputY.current.value = config.params?.y || confDefault.y
+                    inputFontSize.current.value = config.params?.fontsize || confDefault.fontsize
+                    inputFontColor.current.value = config.params?.fontcolor || confDefault.fontcolor
+                    inputButtonLabel.current.value = config.params?.label || confDefault.label
+                    inputButtonLink.current.value = config.params?.link || confDefault.link
                 }}
             >
-                Paste pre-saved
+                Pre-saved configuration
             </Button>
 
-            <Button
-                variant="destructive"
-                className="w-full "
-                onClick={() => {
-                    updateConfig({
-                        video: null,
-                        link: confDefault.video,
-                        label: confDefault.label,
-                        gif: confDefault.gif,
-                        start: null,
-                        finish: null,
-                        caption: null,
-                        fontsize: null,
-                        fps: null,
-                        scale: null,
-                    })
-
-                    logs.current.value = 'The frame has been reseted.'
-                }}
-            >
-                Reset
-            </Button>
         </div>
     )
 }
