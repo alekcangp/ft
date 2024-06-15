aeb200b03fd50e3d65023dccf84ac636a7809732
alekcangp <40195312+alekcangp@users.noreply.github.com>
2024-06-15 13:45:50 +0600

    Update Inspector.tsx

--- templates/gifmaker/Inspector.tsx
+++ templates/gifmaker/Inspector.tsx
@@ -88,9 +88,9 @@ export default function Inspector() {
 				'-t',
 				params.duration,
 				'-r',
-				'8',
+				'7',
 				'-vf',
-				`scale=-1:210,drawtext=fontfile=arial.ttf:text='${params.caption}':fontcolor=${params.fontcolor}:fontsize=${params.fontsize}:x=(w-text_w)/2:y=(h-text_h)-${params.y}`,
+				`scale=-1:200,drawtext=fontfile=arial.ttf:text='${params.caption}':fontcolor=${params.fontcolor}:fontsize=${params.fontsize}:x=(w-text_w)/2:y=(h-text_h)-${params.y}`,
 				'output.gif',
 			]);
 
